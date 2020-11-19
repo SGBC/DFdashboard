2e4bcc015118286dfc9490bddc73f0fdebd69c74
Muhammad Ishfaq <33893160+muhammis@users.noreply.github.com>
2020-11-19 14:02:25 +0200

    Add files via upload
    Based on given 4 csv files, I have made database for the purpose to test the dashboard and also Running Web application.

---
+++ models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.db import *
+
+class Cow_Traffic(models.models):
+   Animal Number(models.IntegerField(max_lenght= 5, blank= False, unique=True)
+   Group Name(models.CharField(max_lenght= 6, blank=False, null=False)
+   Date Time(models.DateTimeField(auto_now=True, auto_now_add=True, blank=False)
+   Previous Area(models.CharField(max_lenght= 50, unique=False)
+   Time Area(models.TimeField(auto_now=True, auto_now_add=True)
+   Device Name(models.CharField(max_length = 100, unique=True)
+   Resutls (models.Results = models.CharField(max_length = 150, unique=True)
+   Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+   Milking Interval (models.Milking Interval = models.IntegerField(max_length=10)
+   Incompletely Milked Teats (models.Incompletely Milked Teats = models.CharField(max_length = 20, blank=True)
+   )
+class FeedDPIDayCow1(models.Model):
+    Animal Id(models.Animal Id = models.IntegerField(max_lenght=5, blank=False, null=False, unique=True)
+    Observation day(models.Observation day = models.DateField(auto_now=True, auto_now_add=True, blank=True, null=True)
+    Feed quantity (models. Feed quantity = models.quantity(max_lenght=10, null=False)
+    Feed number (models.Feed number = models.IntegerField(max_length=5, null=True)
+     Equipment (models. Equipment = models.CharField(max_length = 20, null=False)
+    )
+class  Event List(models.Model):
+    Djurnr (models.Djurnr = models.IntegerField(max_lenght=10, null=False, unique=True)
+    Grupp (models.Grupp = models.CharField(max_length = 150, blank=False, unique=False)
+    Händelsedatum (models.Händelsedatum = models.DateField(auto_now=True, auto_now_add=True)
+    Typ (models.Typ = models.CharField(max_length = 30, blank=False)
+    Beskrivning (models.Beskrivning = models.CharField(max_length = 100, blank=False)
+    Utgångsdatum (models.Utgångsdatum = models.DateField(auto_now=True, auto_now_add=True)
+    
+    
+   
+    
+    )
+
+class GIGACOW_activity_v1_20201102 01(models.Model):
+    Official Reg. No (models.Official Reg. No = models.CharField(max_length = 30, unique=True)
+    Average Activity Last 24h (models.Average Activity Last 24h = models.IntegerField()
+    (max_length = 10, unique=False, null=False)
+    Relative Activity (models.Relative Activity = models.fieldName = models.DecimalField(max_digits=3, decimal_places=2)
+    (max_length = 100, unique=False)
+    Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+    Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+    Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+    Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+    Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+    Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+    Gate Decision(models.Gate Decision = models.CharField(max_length = 100, unique=False)
+    
+    
+    
+    
+    
+    
+    
+    )
+   
+    
+
+    
+
+   
+  
+   
+   
+    
