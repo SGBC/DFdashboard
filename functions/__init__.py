76d3fb8b98271e6d155e1eb18230e2f4b74ae1b8
BjornSeifert <73712145+BjornSeifert@users.noreply.github.com>
2020-12-01 14:53:39 +0100

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,1 +1,6 @@
 import pandas as pd
+from functions.kickOffs import *
+from functions.avg_in_robots import *
+from functions.avg_milking_volume_lact import *
+from functions.num_day_lactation import *
+from functions.preprocess import *
