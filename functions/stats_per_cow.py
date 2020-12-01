6e0060862a92dac8e1975af6b2fed330a88940d6
BjornSeifert <73712145+BjornSeifert@users.noreply.github.com>
2020-12-01 16:23:09 +0100

    Update stats_per_cow.py

--- functions/stats_per_cow.py
+++ functions/stats_per_cow.py
@@ -25,9 +25,9 @@ def cow_stat(animal_id, start_time, milkTime, action, milk, kickOff, animal_id2,
         for j in time:
             temp = re.findall(r'\d+', j)
             times.append(float(temp[0])+float(temp[1])/60)
-        outData.loc[outData.Animal_ID == i, "avg_time"] = round(sum(times)/len(times),2)
+        outData.loc[outData.Animal_ID == i, "avg_Milk_duration"] = round(sum(times)/len(times),2)
         outData.loc[outData.Animal_ID == i, "total_Milk"] = (data[data['Animal_ID']==i])['Milk_yield'].sum()
-        outData.loc[outData.Animal_ID == i, "numberOfkickOffs"] = (outData[outData['Animal_ID']==i])["numberOfMilkings"] -(data[data['Animal_ID']==i])['Nr_of_kickOffs'].isnull().sum()
+        outData.loc[outData.Animal_ID == i, "Nr_of_kickOffs"] = (outData[outData['Animal_ID']==i])["numberOfMilkings"] -(data[data['Animal_ID']==i])['Nr_of_kickOffs'].isnull().sum()
 
     data2 = data2.rename(columns={'Animal Number': 'Animal_ID'})
     outData = pd.merge(outData, data2)
