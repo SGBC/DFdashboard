fb3894921a998f4c831f6f6ad17bbf99a71a1922
BjornSeifert <73712145+BjornSeifert@users.noreply.github.com>
2020-12-01 16:14:49 +0100

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -4,3 +4,4 @@ from functions.avg_in_robots import *
 from functions.avg_milking_volume_lact import *
 from functions.num_day_lactation import *
 from functions.preprocess import *
+from functions.stats_per_cow import *
