d648d69890358393edeb10974a676306130668df
BjornSeifert <73712145+BjornSeifert@users.noreply.github.com>
2020-12-01 15:17:39 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -22,3 +22,5 @@ print('Average 7 days, time in robot: ', func.avg_in_robot(data['Milk_duration']
 print('Average 7 days, milking Volym. ', func.num_day_lactation_0_100(data2['Days In Milk']), ' cows in lactation 0-100: ', func.avg_milking_volume_lact_0_100(data['Date'], data['Animal_ID'], data['Action'], data['Milk_yield'], data3['Animal Number'], data3['Official Reg. No. (ORN)'], data2['Official Reg. No. (ORN)'], data2['Days In Milk']))
 print('Average 7 days, milking Volym. ', func.num_day_lactation_101_200(data2['Days In Milk']), ' cows in lactation 101-200: ', func.avg_milking_volume_lact_101_200(data['Date'], data['Animal_ID'], data['Action'], data['Milk_yield'], data3['Animal Number'], data3['Official Reg. No. (ORN)'], data2['Official Reg. No. (ORN)'], data2['Days In Milk']))
 print('Average 7 days, milking Volym. ', func.num_day_lactation_201_up(data2['Days In Milk']), ' cows in lactation 200-: ', func.avg_milking_volume_lact_201_up(data['Date'], data['Animal_ID'], data['Action'], data['Milk_yield'], data3['Animal Number'], data3['Official Reg. No. (ORN)'], data2['Official Reg. No. (ORN)'], data2['Days In Milk']))
+
+#data.to_csv("testdata.csv",index=False)
