3f9e1e1a3f46f6f67fbaf8cc1a09cdc36c553363
axeljjj <74040500+axeljjj@users.noreply.github.com>
2020-12-02 15:22:45 +0100

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -5,3 +5,4 @@ from functions.milk_per_cow import *
 from functions.milking_cows_yesterday import *
 from functions.num_day_lactation import *
 from functions.preprocess import *
+from functions.projected_monthly_milk import *
