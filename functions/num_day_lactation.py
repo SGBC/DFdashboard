0e531a61b5f96d5953ae635a2dfe7fcb196da2a1
BjornSeifert <73712145+BjornSeifert@users.noreply.github.com>
2020-12-28 11:11:11 +0100

    Update num_day_lactation.py

--- functions/num_day_lactation.py
+++ functions/num_day_lactation.py
@@ -10,7 +10,21 @@ def num_day_lactation_0_100(DaysInMilk):
 def num_day_lactation_101_200(DaysInMilk):
     return (DaysInMilk[DaysInMilk<=200]>100).sum()
 
-# Input: pandas DataFrame for DaysInMilk
+# Input: pandas DataFrame for animal_id, milk, animal_id2, off_id, off_id2, DaysInMilk
 # Output: Number off cows who has been in milk more than 200 days
-def num_day_lactation_201_up(DaysInMilk):
-    return (DaysInMilk>200).sum()
+def num_day_lactation_201_up(animal_id, milk, animal_id2, off_id, off_id2, DaysInMilk):
+        
+    # Concatenate data
+    data = pd.concat([animal_id, milk], axis = 1)
+    data2 = pd.concat([animal_id2, off_id], axis = 1)
+    data3 = pd.concat([off_id2, DaysInMilk], axis = 1)
+
+    # Removes all cows who doesn't fit the criteria
+    data3 = data3[data3['Days In Milk'] > 200]  
+    data2 = data2[data2['Official Reg. No. (ORN)'].isin(data3['Official Reg. No. (ORN)'])]
+    data = data[data['Animal_ID'].isin(data2['Animal Number'])]
+
+    # Get the unique dates and cows
+    cow_id = pd.unique(data['Animal_ID'])
+    
+    return (cow_id >= 0).sum()
