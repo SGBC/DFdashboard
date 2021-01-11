a4aba21bd4d1ecf7ef4f62b5721b052fcb5e6ea1
BjornSeifert <73712145+BjornSeifert@users.noreply.github.com>
2021-01-11 14:39:39 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -44,7 +44,7 @@ for of in range(9):
     #print(data)
     #print(dataA2)
 
-    #data_cow = func.daily_milk_per_cow(data['Date'], data['Animal_ID'], data['Action'], data['Milk_yield'])
+    data_cow = func.daily_milk_per_cow(data['Date'], data['Animal_ID'], data['Action'], data['Milk_yield'])
 
     #data_cow.to_csv("data_cow.csv", index=False, sep = ';')
 
