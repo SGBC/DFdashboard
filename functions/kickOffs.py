3550ce91508378445d79186ca1944ab0fb50e112
axeljjj <74040500+axeljjj@users.noreply.github.com>
2021-01-13 13:38:26 +0100

    Delete kickOffs.py

--- functions/kickOffs.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-# Input: pandas DataFrame for start_time, kickOff
-# Output: Average kickOffs based on the number of dates present in the data
-def avg_kickOffs(start_time, Animal_ID, kickOff):
-    return round(kickOff.sum()/(len(pd.unique(start_time)) + len(pd.unique(start_time))),2)
